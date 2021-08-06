c1aded67774ba452c64608b068d58573c10c164a
4674 <yaouyao@sina.cn>
2021-08-06 16:04:18 +0800

    0.1

---
+++ 1.两数之和.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        nums;
+        return vector<int>{1};
+    }
+};
+    
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
+
+// @lc code=end
